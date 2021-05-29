e8bada67bd7dccb1fb11c78040ae3f5409bfd03d
heredia lucas <heredialucasfac22@hotmail.com>
2021-05-29 02:31:08 -0300

    Mejorando Portafolio

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+    const navBtn = document.getElementById('navbarSupportedContent');
+    const navHide = () => document.getElementById('navbarSupportedContent').classList.remove('show');
+    const navMin = () => document.querySelector('.navbar').style.width = '18vw';
+    const navRadius = () => document.querySelector('.navbar').style.borderRadius = '0px 0px 23px 0px';
+
+    navBtn.addEventListener ('click', (e) => {
+            
+        if(e.target.classList.contains('nav-link')) {
+            navHide();
+            navMin();
+            navRadius();
+        }
+    });
+    
+    document.body.addEventListener('click', () => navHide());
+})
