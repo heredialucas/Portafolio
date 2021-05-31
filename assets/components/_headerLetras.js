f8ef66740b2093f5991281684479db61a37944df
heredia lucas <heredialucasfac22@hotmail.com>
2021-05-31 18:38:24 -0300

    Probando Scripts

--- assets/components/_headerLetras.js
+++ assets/components/_headerLetras.js
@@ -1,44 +1,44 @@
-// export default class Letras {
-//     constructor() {
-//         this.letras = document.querySelector('.cabecera__div--h1--p1');
-//         this.titulos = document.querySelector('.expandir');
+export default class Letras {
+    constructor() {
+        this.letras = document.querySelector('.cabecera__div--h1--p1');
+        this.titulos = document.querySelector('.expandir');
 
-//         const maquinaEscribir1 = (text = '',tiempo = '', etiqueta = '') => {
-//             let arrayCaracteres = text.split('')
-//             etiqueta.innerHTML = ''
-//             let cont = 0
-//             let escribir = setInterval(function(){
-//               etiqueta.innerHTML += arrayCaracteres[cont]
-//               cont++
-//               if (cont === arrayCaracteres.length) {
-//                 clearInterval(escribir)
-//               }
-//             }, tiempo)
-//           }
+        const maquinaEscribir1 = (text = '',tiempo = '', etiqueta = '') => {
+            let arrayCaracteres = text.split('')
+            etiqueta.innerHTML = ''
+            let cont = 0
+            let escribir = setInterval(function(){
+              etiqueta.innerHTML += arrayCaracteres[cont]
+              cont++
+              if (cont === arrayCaracteres.length) {
+                clearInterval(escribir)
+              }
+            }, tiempo)
+          }
 
-//         // const maquinaEscribir2 = (text = '',tiempo = '200', etiqueta = '') => {
-//         //     let arrayCaracteres = text.split('');
-//         //     etiqueta.innerHTML = '';
-//         //     let i = 0;
-//         //     let j = text.length;
-//         //     let escribir = setInterval(function(){
-//         //       if (i === arrayCaracteres.length) {
-//         //         etiqueta.innerHTML = text.substring(0,j);
-//         //         j--;
-//         //         if (j === 0) {
-//         //           etiqueta.innerHTML = '';
-//         //           i = 0;
-//         //           j = text.length;
-//         //         }
-//         //       } else {
-//         //         etiqueta.innerHTML += arrayCaracteres[i];
-//         //         i++;
-//         //       }
-//         //     }, tiempo);
-//         //   }
+        // const maquinaEscribir2 = (text = '',tiempo = '200', etiqueta = '') => {
+        //     let arrayCaracteres = text.split('');
+        //     etiqueta.innerHTML = '';
+        //     let i = 0;
+        //     let j = text.length;
+        //     let escribir = setInterval(function(){
+        //       if (i === arrayCaracteres.length) {
+        //         etiqueta.innerHTML = text.substring(0,j);
+        //         j--;
+        //         if (j === 0) {
+        //           etiqueta.innerHTML = '';
+        //           i = 0;
+        //           j = text.length;
+        //         }
+        //       } else {
+        //         etiqueta.innerHTML += arrayCaracteres[i];
+        //         i++;
+        //       }
+        //     }, tiempo);
+        //   }
 
-//           maquinaEscribir1('Heredia Lucas()', 100, this.letras);
-//           // maquinaEscribir2('Acerca de Mi', 100, this.titulos);
-//     }
-// };
+          maquinaEscribir1('Heredia Lucas()', 100, this.letras);
+          // maquinaEscribir2('Acerca de Mi', 100, this.titulos);
+    }
+};
 
--- assets/components/main.js
+++ assets/components/main.js
@@ -1,22 +1,41 @@
-import  NavHide from './_navHide.js';
 // import Letras from './_headerLetras.js';
+// import  NavHide from './_navHide.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
     
     // const letras = new Letras();
-    const navHide = new NavHide();
+    // const navHide = new NavHide();
 
-        
-        
-    // const navBtn = document.getElementById('navbarSupportedContent');
-    // const navHide = () => document.getElementById('navbarSupportedContent').classList.remove('show');
-    // navBtn.addEventListener ('click', (e) => {
+    const navBtn = document.getElementById('navbarSupportedContent');
+    const navHide = () => document.getElementById('navbarSupportedContent').classList.remove('show');
+   
+    navBtn.addEventListener ('click', (e) => {
             
-    //     if(e.target.classList.contains('nav-link')) {
-    //         navHide();
-    //     }
-    // });
+        if(e.target.classList.contains('nav-link')) {
+            navHide();
+        }
+    });
+
     
     
+    letras = document.querySelector('.cabecera__div--h1--p1');
+    titulos = document.querySelector('.expandir');
+    
+    const maquinaEscribir1 = (text = '',tiempo = '', etiqueta = '') => {
+        let arrayCaracteres = text.split('')
+        etiqueta.innerHTML = ''
+        let cont = 0
+        let escribir = setInterval(function(){
+            etiqueta.innerHTML += arrayCaracteres[cont]
+            cont++
+            if (cont === arrayCaracteres.length) {
+                clearInterval(escribir)
+            }
+        }, tiempo)
+    }
+    
+    
+    document.body.addEventListener('click', () => navHide());
+    maquinaEscribir1('Heredia Lucas()', 100, this.letras);
 });
